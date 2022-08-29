1eaba66ccd8548b276456232195b6d5cd631cfe6
AgnidDrage <marianosancheztoledo@gmail.com>
2022-08-29 08:54:57 -0300

    Update

--- TP2/main.py
+++ TP2/main.py
@@ -2,7 +2,8 @@ from perceptron import Perceptron
 
 
 def main():
-    useOr = True  # True for OR, False for AND
+    useTable = "OR"  # True for OR, False for AND.
+    stopFlag = False # True for stop when error < 10% , False for continue.
     index = 0
     perceptron = Perceptron()
     orTable = [
@@ -19,8 +20,15 @@ def main():
         [1, 1, 1, 1]
     ]
 
+    xorTable = [
+        [0, 0, 0, 0],
+        [0, 1, 1, 1],
+        [1, 0, 1, 1],
+        [1, 1, 0, 0]
+    ]
+
     # Train the perceptron with the table.
-    if useOr:
+    if useTable == "OR":
         for i in range(300):
             row = orTable[index]
             index += 1
@@ -28,7 +36,10 @@ def main():
                 index = 0
             perceptron.processInput(row, row[3])
             perceptron.updateWeights()
-    else:
+            if perceptron.error < 0.1 and stopFlag == True:
+                print("Iteration to have less than 10% error: ", i)
+                break
+    elif useTable == "AND":
         for i in range(300):
             row = andTable[index]
             index += 1
@@ -36,15 +47,27 @@ def main():
                 index = 0
             perceptron.processInput(row, row[3])
             perceptron.updateWeights()
+            if perceptron.error < 0.1 and stopFlag == True:
+                print("Iteration to have less than 10% error: ", i)
+                break
+    else:
+        for i in range(300):
+            row = xorTable[index]
+            index += 1
+            if index == len(xorTable):
+                index = 0
+            perceptron.processInput(row, row[3])
+            perceptron.updateWeights()
+            if perceptron.error < 0.1 and stopFlag == True:
+                print("Iteration to have less than 10% error: ", i)
+                break
 
     # Test prediction.
     print("Let's predict, please enter 3 numbers without space, for example: 111")
     while True:
         values = formatInput(input('Enter values: '))
-        print(values)
         for i in range(len(values)):
             values[i] = int(values[i])
-        print(values)
         output = perceptron.predict(values)
         print('Output: ',   round(output))
 
--- TP2/perceptron.py
+++ TP2/perceptron.py
@@ -4,13 +4,14 @@ import math
 
 class Perceptron:
     def __init__(self):
-        # w1 = random.uniform(-1, 1)
-        # w2 = random.uniform(-1, 1)
-        # w3 = random.uniform(-1, 1)
-        # self.weights = [w1, w2, w3]
-        self.weights = [0, 0, 0]
+        w1 = random.uniform(-1, 1)
+        w2 = random.uniform(-1, 1)
+        w3 = random.uniform(-1, 1)
+        self.weights = [w1, w2, w3]
+        # self.weights = [0, 0, 0]
         self.inputs = []
         self.LR = 0.1
+        self.error = 0
         self.spectedOutput = 0  # spected output
         self.realOutput = 0  # real output
 
@@ -24,8 +25,8 @@ class Perceptron:
         self.realOutput = self.__activationFunction(sumVal)
 
     def updateWeights(self):
-        error = self.spectedOutput - self.realOutput
-        delta = self.LR * error
+        self.error = self.spectedOutput - self.realOutput
+        delta = self.LR * self.error
         for i in range(len(self.weights)):
             deltaW = delta * self.inputs[i]
             self.weights[i] += deltaW
@@ -37,18 +38,17 @@ class Perceptron:
             c1 = inputs[1] * self.weights[1]
             c2 = inputs[2] * self.weights[2]
             sumVal = c0 + c1 + c2
-            output = self.__activationFunction(sumVal)
-            print("inside output", output)
             return self.__activationFunction(sumVal)
         else:
             raise ValueError(
                 "Inputs must be 3 bits without space, example: 111")
 
     def __activationFunction(self, sumVal):
-        if sumVal > 0:
-            return 1
-        else:
-            return 0
+        # if sumVal > 0:
+        #     return 1
+        # else:
+        #     return 0
+        return 1 / (1 + math.e ** (-sumVal))
 
     def __str__(self):
         return str(self.weights)
